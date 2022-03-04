b24c5dfff8f1e94f3547d67c1d4ca42663b8e448
JaehyeonPaak <91472722+JaehyeonPaak@users.noreply.github.com>
2022-03-05 02:13:17 +0900

    add, mul function

--- src/Recitation_02/Polynomial.java
+++ src/Recitation_02/Polynomial.java
@@ -6,10 +6,10 @@ public class Polynomial {
     public Polynomial(double[] coef) { //constructor...
         //trim the leading zeros
         int n = coef.length;
-        while(n >= 2 && coef[n-1] == 0)
+        while(n >= 2 && coef[n-1] == 0) //if coef = [1, 1, 0], remove 0...
             n--;
 
-        this.coef = new double[n];
+        this.coef = new double[n]; //declare an array whose size is n...
         for(int i = 0; i < n; i++)
             this.coef[i] = coef[i];
     }
@@ -24,14 +24,28 @@ public class Polynomial {
         return sb.toString();
     }
     public Polynomial add(Polynomial that) {
-        double[] c = new double[Math.max(this.coef.length, that.coef.length)];
+        double[] c = new double[Math.max(this.coef.length, that.coef.length)]; //Math.max(a, b) == a (if a >= b)...
         //TODO implement add
-        return null;
+        for(int i = c.length - 1; i >= 0; i--) {
+            c[i] = this.coef[i] + that.coef[i];
+        }
+        return new Polynomial(c);
     }
-    public Polynomial mul(Polynomial that) {
-        double[] c = new double[this.coef.length + that.coef.length - 1];
+    public Polynomial mul(Polynomial that) { // (x + 1) * (x - 1) / {1, 1} {-1, 1}...
+        double[] c = new double[this.coef.length + that.coef.length - 1]; // c.length == 3...
         //TODO implement mul
-        return null;
+        for(int i = c.length - 1; i >= 0; i--) { //2, 1, 0...
+            if(i == 2) {
+                c[i] = this.coef[i - 1] * that.coef[i - 1];
+            }
+            else if(i == 1) {
+                c[i] = (this.coef[i - 1] * that.coef[i]) + (this.coef[i] * this.coef[i - 1]);
+            }
+            else {
+                c[i] = this.coef[i] * that.coef[i];
+            }
+        }
+        return new Polynomial(c);
     }
     public Polynomial[] longdiv(Polynomial that) {
         //return value: longdiv(...)[0]: quotient,
@@ -51,20 +65,20 @@ public class Polynomial {
         return null;
     }
     public static void main(String[] args) {
-        Polynomial a = new Polynomial(new double[] {-1, 1});
+        Polynomial a = new Polynomial(new double[] {-1, 1}); //x - 1...
         System.out.println("a: " + a);
 
-        Polynomial b = new Polynomial(new double[] { 1, 1});
+        Polynomial b = new Polynomial(new double[] { 1, 1}); //x + 1...
         System.out.println("b: " + b);
 
         Polynomial c = a.add(b);
-        System.out.println("c = (a + b): " + c);
+        System.out.println("c = (a + b): " + c); //2x...
 
         Polynomial d = a.mul(b);
-        System.out.println("d = (a * b): " + d);
+        System.out.println("d = (a * b): " + d); //x^2 - 1...
 
         Polynomial e = d.add(c);
-        System.out.println("e = (d + c): " + e);
+        System.out.println("e = (d + c): " + e); //x^2 + 2x + 1...
 
         Polynomial[] f = e.longdiv(a);
         System.out.println("e / a: " + f[0]);
